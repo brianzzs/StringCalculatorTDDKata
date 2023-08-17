dc34a1e1adf464d8695652796d2cc0d6469c7ff3
brianzzs <brianzanoni@outlook.com>
2023-08-17 17:49:51 -0300

    refactor stage and newline support

--- StringCalculatorKata.Tests/StringCalculatorTests.cs
+++ StringCalculatorKata.Tests/StringCalculatorTests.cs
@@ -9,8 +9,9 @@ public class StringCalculatorTests
     [InlineData("", 0)]
     [InlineData("1", 1)]
     [InlineData("1,2", 3)]
-    [InlineData("1,2,3", 6)]
-    public void ShouldReturnNumber(string numbers, int expectedNumber)
+    [InlineData("1\n2,3", 6)]
+
+    public void ShouldReturnNumberOrSum(string numbers, int expectedNumber)
     {
         var calc = new StringCalculator();
         var result = calc.Add(numbers);
--- StringCalculatorKata/Calculator.cs
+++ StringCalculatorKata/Calculator.cs
@@ -5,19 +5,26 @@ public class StringCalculator
     public int Add(string numbers)
     {
         int result = 0;
-        if(string.IsNullOrEmpty(numbers))
+        bool isStringEmpty = string.IsNullOrEmpty(numbers);
+        bool numbersContainsNewLine = numbers.Contains("\n");
+        bool numbersContainsComma = numbers.Contains(",");
+
+        if (isStringEmpty)
             return 0;
 
-        if(numbers.Contains(","))
+        if (numbersContainsNewLine)
+            numbers = numbers.Replace("\n", ",");
+
+        if (numbersContainsComma)
         {
             var numbersArray = numbers.Split(',');
-            foreach(var number in numbersArray)
+            foreach (var number in numbersArray)
             {
                 result += int.Parse(number);
             }
             return result;
         }
-
-        return int.Parse(numbers);
+        else
+            return int.Parse(numbers);
     }
 }
